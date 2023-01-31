4fc16fc7390283f839126c0a6c61e8b0649e6c88
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-01-31 22:33:10 +0200

    New Update

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+/* Copy Menu FOr Mobile */
+function copyMenu() {
+  // copy inside .dpt-cart to .departments
+  let dptCategory = document.querySelector(".dpt-cart");
+  let dptPlace = document.querySelector(".departments");
+  dptPlace.innerHTML = dptCategory.innerHTML;
+
+  //Copy inside nav to nav
+  let mainNav = document.querySelector(".header-nav nav");
+  let navPlace = document.querySelector(".off-canvas nav");
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  //Copy >header-top .wrapper to .theTop-nav
+  let topNav = document.querySelector(".header-top .wrapper");
+  let topPlace = document.querySelector(".off-canvas .thetop-nav");
+  topPlace.innerHTML = topNav.innerHTML;
+}
+copyMenu();
