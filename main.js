e06bbc2d0ddda7b9f350f536df8e46ca366595df
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-12 23:36:52 +0200

    New Update

--- main.js
+++ main.js
@@ -94,3 +94,14 @@ let productBig = new Swiper(".big-image", {
     swiper: ProductThumb,
   },
 });
+
+// stock products bar width percentage
+let stocks = document.querySelectorAll(".products .stock");
+
+for (let x = 0; x < stocks.length; x++) {
+  let stock = stocks[x].dataset.stock,
+    available = stocks[x].querySelector(".qty-available").innerHTML,
+    sold = stocks[x].querySelector(".qty-sold").innerHTML,
+    percent = (sold * 100) / stock;
+  stocks[x].querySelector(".available").style.width = percent + "%";
+}
