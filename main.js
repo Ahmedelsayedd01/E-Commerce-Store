14238435ccaaff92212b24ea37b439fb8f4b01ee
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-16 23:30:53 +0200

    Last Update

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ let stocks = document.querySelectorAll(".products .stock");
 for (let x = 0; x < stocks.length; x++) {
   let stock = stocks[x].dataset.stock,
     available = stocks[x].querySelector(".qty-available").innerHTML,
-    sold = stocks[x].querySelector(".qty-sold").innerHTML.valueOf,
+    sold = stocks[x].querySelector(".qty-sold").innerHTML,
     percent = (sold * 100) / stock;
   stocks[x].querySelector(".available").style.width = percent + "%";
 }
